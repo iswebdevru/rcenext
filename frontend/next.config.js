593556a390e0215c942f43f1a2e787d350451105
wardxela <axelwardweb@gmail.com>
2023-05-18 19:01:15 +0300

    feat: setup dev environment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    // Enable polling based on env variable being set
+    if (process.env.NEXT_ENABLE_POLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
