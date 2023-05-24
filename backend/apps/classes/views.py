13032290584176febe9c11de9df6d65dd963fcdf
wardxela <axelwardweb@gmail.com>
2023-05-24 14:34:36 +0300

    feat: add pagination for classes resource

--- backend/apps/classes/views.py
+++ backend/apps/classes/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Q, Exists, OuterRef
-from rest_framework import viewsets
-from rest_framework.response import Response
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import ClassesSchedule
 from .serializers import ClassesScheduleMixedSerializer
@@ -8,72 +7,44 @@ from .validators import validate_classes_query_params
 from .service import get_day_info
 
 
-class ClassesScheduleViewSet(viewsets.ViewSet):
+class ClassesScheduleViewSet(
+    viewsets.GenericViewSet,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin
+):
     queryset = ClassesSchedule.objects.all()
+    serializer_class = ClassesScheduleMixedSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def list(self, request):
+        queryset = self.get_queryset()
         query_params = validate_classes_query_params(request)
         schedule_type = query_params['type']
-        if (schedule_type == 'main'):
+        if schedule_type == 'main':
             week_day = query_params['week_day']
             week_type = query_params['week_type']
-            return Response(ClassesScheduleMixedSerializer(
-                self.queryset.filter(
-                    type=ClassesSchedule.ScheduleType.MAIN,
-                    week_day=week_day,
-                    week_type=week_type
-                ),
-                many=True,
-                context={
-                    'request': request
-                }).data,
+            queryset = queryset.filter(
+                type=ClassesSchedule.ScheduleType.MAIN,
+                week_day=week_day,
+                week_type=week_type
             )
-        date = query_params['date']
-        if (schedule_type == 'changes'):
-            return Response(
-                ClassesScheduleMixedSerializer(
-                    self.queryset.filter(
-                        type=ClassesSchedule.ScheduleType.CHANGES,
-                        date=date
-                    ),
-                    many=True,
-                    context={
-                        'request': request
-                    }
-                ).data
+        elif schedule_type == 'changes':
+            date = query_params['date']
+            queryset = queryset.filter(
+                type=ClassesSchedule.ScheduleType.CHANGES,
+                date=date
             )
-        week_type, week_day = get_day_info(date)
-        return Response(
-            ClassesScheduleMixedSerializer(
-                self.queryset.filter(
-                    Q(date=date) |
-                    Q(week_day=week_day) & Q(week_type=week_type) &
-                    ~Exists(self.queryset.filter(
-                        Q(group=OuterRef('group')) & Q(date=date))
-                    )
-                ),
-                many=True,
-                context={
-                    'request': request
-                }
-            ).data
-        )
-
-    def create(self, request):
-        classes_serializer = ClassesScheduleMixedSerializer(
-            data=request.data,
-            context={
-                'request': request
-            }
-        )
-        classes_serializer.is_valid(raise_exception=True)
-        classes_serializer.save()
-        return Response(classes_serializer.data)
-
-    def retrieve(self, request, pk=None):
-        return Response(ClassesScheduleMixedSerializer(self.queryset.get(id=pk), context={'request': request}).data)
-
-    def destroy(self, request, pk=None):
-        self.queryset.get(id=pk).delete()
-        return Response({'status': 'success'})
+        else:
+            date = query_params['date']
+            week_type, week_day = get_day_info(date)
+            queryset = queryset.filter(
+                Q(date=date) |
+                Q(week_day=week_day) & Q(week_type=week_type) &
+                ~Exists(queryset.filter(
+                    Q(group=OuterRef('group')) & Q(date=date))
+                )
+            )
+        page = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
